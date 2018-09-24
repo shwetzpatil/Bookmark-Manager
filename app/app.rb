adc023aa21ac4971fdbf66d1186c6cd07d60129c
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-09-24 15:14:55 +0100

    sets config and spec helpers

---
+++ app/app.rb
@@ -0,0 +1,9 @@
+require 'sinatra/base'
+
+class Bookmark < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb(:index)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+ENV['RACK_ENV'] = 'test'
+
+require '../app/controllers/app'
+require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+Capybara.app = Bookmark
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
